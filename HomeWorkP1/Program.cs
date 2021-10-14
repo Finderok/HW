cc706e878a94b1be3634006b62014ba81c2a6c40
TimelessRaven <msamohval@gmail.com>
2021-10-14 23:04:49 +0300

    HW P1

---
+++ HomeWorkP1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWorkP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsEarthFlat = false;
+            byte AnswerToUltimateQ = 42;
+            sbyte WheelsInCar = 4;
+            char Dollar = '$';
+            decimal PepperoniPizza = 7;
+            double Pi = 3.14;
+            float AbsoluteZero = 459.67f;
+            int EyeNumber = 2;
+            uint Million = 1000000;
+            long MinusMillion = -1000000;
+            ulong JeffBezosNet = 13000000000;
+            short Border = -32768;
+            string NewVenom = "Wasted money";
+            object AnotherPi = 3.141;
+        }
+    }
+}
