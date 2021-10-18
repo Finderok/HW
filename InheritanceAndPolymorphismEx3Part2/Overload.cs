16f21749d473a4c061f0d856fd6f00a3090f9254
TimelessRaven <msamohval@gmail.com>
2021-10-19 00:22:52 +0300

    HomeWork №2 Ex3 P2

---
+++ InheritanceAndPolymorphismEx3Part2/Overload.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InheritanceAndPolymorphismEx3Part2
+{
+    public class Overload
+    {
+        public void Display()
+        {
+            DisplayOverload(100, "200", "300");
+            DisplayOverload(200, "100");
+
+            string[] names = { "Akhil", "Ekta", "Arsh" };
+            DisplayOverload(200, names);
+        }
+
+        private void DisplayOverload(int a, params string[] parameterArray)
+        {
+            foreach (var i in parameterArray)
+                Console.WriteLine(i + " " + a);
+        }
+    }
+}
---
+++ InheritanceAndPolymorphismEx3Part2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InheritanceAndPolymorphismEx3Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var overload = new Overload();
+            overload.Display();
+        }
+    }
+}
