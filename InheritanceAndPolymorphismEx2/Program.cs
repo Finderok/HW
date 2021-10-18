98148473bbda596322b094037266977e9012cdbb
TimelessRaven <msamohval@gmail.com>
2021-10-19 00:20:01 +0300

    HomeWork №2 Ex2

---
+++ InheritanceAndPolymorphismEx2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InheritanceAndPolymorphismEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var overload = new Overload();
+            overload.Display();
+            overload.Display3();
+        }
+    }
+}
