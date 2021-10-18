1233a6df1239858d9013f3cf3aaec01e8f2680e8
TimelessRaven <msamohval@gmail.com>
2021-10-19 00:27:12 +0300

    HomeWork №2 Ex5

---
+++ InheritanceAndPolymorphismEx5/Overload.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InheritanceAndPolymorphismEx5
+{
+    public class Overload
+    {
+        public void Display()
+        {
+            DisplayOverload(200);
+            DisplayOverload(200, 300);
+            DisplayOverload(200, 300, 500, 600);
+        }
+
+        private void DisplayOverload(int x,int y)
+        {
+            Console.WriteLine("The two integers " + x + " " + y);
+        }
+
+        private void DisplayOverload(params int[] parameterArray)
+        {
+            Console.WriteLine("parameterArray");
+        }
+    }
+}
