787a58b56dc3a3ce56b81e66f302b6ce71d73dd6
TimelessRaven <msamohval@gmail.com>
2021-10-19 00:23:55 +0300

    HomeWork №2 Ex4 P1

---
+++ InheritanceAndPolymorphismEx3Part2/InheritanceAndPolymorphismEx4/Overload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace InheritanceAndPolymorphismEx4
+{
+    public class Overload
+    {
+        public void Display()
+        {
+            int[] numbers = { 10, 20, 20 };
+            DisplayOverload(40, numbers);
+            Console.WriteLine(numbers[1]);
+        }
+        private void DisplayOverload(int a, params int[] parameterArray)
+        {
+            parameterArray[1] = 3000;
+        }
+    }    
+}
